7a35fd24b981928a51ec3a8284897bc56ba3f0da
Patrick Ranz <patrick.ranz@anecon.com>
2017-06-05 15:28:23 +0200

    api key for gmaps added; map is already being displayed but the height and width is not correct yet

--- app/src/main/java/at/tripic/Main.java
+++ app/src/main/java/at/tripic/Main.java
@@ -3,13 +3,23 @@ package at.tripic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+//public class Main extends AppCompatActivity {
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
-
-        System.out.println("paddy paddy paddy");
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
     }
+
+
 }
