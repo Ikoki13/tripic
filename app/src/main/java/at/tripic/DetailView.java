0a0ec6ca6918071bbd16ac68876f9e206e124344
Patrick Ranz <patrick.ranz@anecon.com>
2017-06-06 18:32:04 +0200

    made edit texts and ratingbars non editable

--- app/src/main/java/at/tripic/DetailView.java
+++ app/src/main/java/at/tripic/DetailView.java
@@ -7,13 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class DetailView extends AppCompatActivity {
+
+    EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
+
+        et1 = (EditText) findViewById(R.id.spot_1_desc);
+        et1.setKeyListener(null);
     }
     public void navigateBack(View view) {
         Intent intent = new Intent(this, MapViewOverview.class);
--- app/src/main/java/at/tripic/PictureView.java
+++ app/src/main/java/at/tripic/PictureView.java
@@ -7,17 +7,37 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class PictureView extends AppCompatActivity {
+
+    EditText et1;
+    EditText et2;
+    EditText et3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_view);
+
+        et1 = (EditText) findViewById(R.id.spot_1_desc);
+        et1.setKeyListener(null);
+        et2 = (EditText) findViewById(R.id.spot_2_desc);
+        et2.setKeyListener(null);
+        et3 = (EditText) findViewById(R.id.spot_3_desc);
+        et3.setKeyListener(null);
+
+
     }
 
     public void navigateBack(View view) {
         Intent intent = new Intent(this, MapViewOverview.class);
+        startActivity(intent);
+    }
+
+    public void openPopupView(View view) {
+        Intent intent = new Intent(this, PopupView.class);
         startActivity(intent);
     }
 
