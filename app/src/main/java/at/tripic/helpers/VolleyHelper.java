ac6baa4cb8e35f56ebdc9168b09787c2a0808bef
Ikoki13 <michi.mosgoeller@gmail.com>
2017-06-14 22:56:42 +0200

    add volley helper

---
+++ app/src/main/java/at/tripic/helpers/VolleyHelper.java
@@ -0,0 +1,52 @@
+package at.tripic.helpers;
+
+import android.app.Application;
+import android.app.DownloadManager;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+/**
+ * Created by michael on 14/06/2017.
+ */
+
+public class VolleyHelper {
+
+    private static VolleyHelper instance = null;
+    private static RequestQueue requestQueue = null;
+
+    private VolleyHelper() {
+    }
+
+    private VolleyHelper(Context currentContext) {
+        requestQueue = Volley.newRequestQueue(currentContext);
+    }
+
+    public static VolleyHelper getInstance(Context currentContext) {
+
+        if(instance == null)
+            instance = new VolleyHelper(currentContext);
+
+        return instance;
+    }
+
+    public <T> void addRequestToQueue(Request<T> newRequest) {
+        requestQueue.add(newRequest);
+    }
+
+    public StringRequest createVolleyRequest(String url, int method, Response.Listener<String> success, Response.ErrorListener<String> error) {
+        return new StringRequest(method, url, success, error);
+    }
+
+    public JsonObjectRequest createJsonObjectRequest(String url, int method, JSONObject jsonRequest, Response.Listener<JSONObject> success, Response.ErrorListener error) {
+        return new JsonObjectRequest(method, url, jsonRequest, success, error);
+    }
+}
